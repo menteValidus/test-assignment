c719b5585f7427d90e9bf027a63e36528bf65334
menteVali <denis.chernyy888@gmail.com>
2019-11-25 21:47:07 +0300

    Added preferences screen.

--- app/src/main/java/mente/vali/dailyweather/presentation/ui/MainActivity.kt
+++ app/src/main/java/mente/vali/dailyweather/presentation/ui/MainActivity.kt
@@ -1,6 +1,9 @@
 package mente.vali.dailyweather.presentation.ui
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProviders
 import mente.vali.dailyweather.R
@@ -26,6 +29,24 @@ class MainActivity : AppCompatActivity() {
             ViewModelProviders.of(this).get(ForecastViewModel::class.java)
 
         setContentView(R.layout.activity_main)
+
+        supportActionBar?.title = "Погода"
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.app_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.settings_menu_point -> {
+                startActivity(Intent(this, SettingsActivity::class.java))
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+
     }
 
 
